fca559f9806eae903c0d250332631f8b4e6fe858
Eduardo Laguna <wrcdriver@gmail.com>
2013-04-13 21:24:23 -0430

    Commit Inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TIS::Application.config.secret_token = '26eb7014bfb9198a41327f4b178d5dc40d64c7540153f3e02d874d9a737214279f8e489bfc1e2b8765ad1cd0b1e5a238242167376445963a929456e3d876b75f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+TIS::Application.config.session_store :cookie_store, key: '_TIS_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# TIS::Application.config.session_store :active_record_store
